ec62d3423ecc5532f218313b52e7d403f3d32fbe
rickzinguh <106189527+rickzinguh@users.noreply.github.com>
2022-09-27 16:54:47 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,1 +1,7 @@
+//alert("oi filha eu sou o alert")
+//console.log ("oi, eu estou no console")
+//document.write("Aula de pens, computacional")
 
+function meChame(){
+    alert (alert("oi filha eu sou o alert"))
+}
